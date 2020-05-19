8ee6adab803bdce9166e81cc72705a0f2f2603fa
aa <aa@aztek.xyz>
2020-05-18 22:58:27 -0700

    Push to add password_gen.py

---
+++ password_gen.py
@@ -0,0 +1,30 @@
+# AA Test, Password generator
+
+import sys
+import random
+import string
+import secrets
+from colorama import Fore, Back, Style
+
+intro =\
+"""
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+|                                     AA Password Generator	                                        |
+ |                                      - Version: 1.0 -                                                |
+ |                                   - By aa@aztek.xyz -                                               |
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+int_arg = 0
+try:
+    int_arg = int(sys.argv[1], base=10)
+
+except:
+    print(Fore.RED + "ERROR! Please only enter a base 10 number!")
+
+def randomstring(stringlength):
+    securestr = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for char in range(stringlength)))
+    return securestr
+
+user_choice = int(int_arg)
+print(randomstring(user_choice))
